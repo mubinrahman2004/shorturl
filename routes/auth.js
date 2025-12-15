d00ae62eee6b5533d4ab73541208fce99821c74e
mubinrahman2004 <mubinrahman2003@gmail.com>
2025-12-15 15:40:09 +0600

    middelewire

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ const { signup, login } = require("../Controllers/authController");
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login",  login);
 
 module.exports = router;
